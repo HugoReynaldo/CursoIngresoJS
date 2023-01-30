1389d9115272b036b0f652086c547a535264e7bb
HugoReynaldo <pattodroi@gmail.com>
2023-01-30 16:36:48 -0300

    vercion 3

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,15 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	//reserva espacio en memoria para guardar nombre del usuario
+	let nombre;
+
+	//guardo en la variable el nombre que escribio el usuario de la ventana prompt
+	nombre = prompt("ingrese su nombre");
+
+	// buscamos un id que no se encuentra en la pagina js y que lo podemos encontar en la pag de html.
+	document.getElementById(txtIdNombre).value = nombre;
+
+	alert(nombre)
 }
 
