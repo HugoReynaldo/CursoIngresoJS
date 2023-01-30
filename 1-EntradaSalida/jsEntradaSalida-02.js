ad78a29bdc3557ec148d28a9c5c50ec3a938db40
HugoReynaldo <pattodroi@gmail.com>
2023-01-30 08:19:06 -0300

    nuva version

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,49 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+
+	let nombre;
+
+	nombre = prompt("Ingrese su nombre")
+
+	alert(nombre);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let es una variable, tambien debo poner algun nombre de la varieble. Es para indentificar la variable.
+let nombre = "jose"
+	
+alert(nombre);
+
+//Inicialido de variable. "=" Es un operardor de asignacion, lo que va a la derecha es el origen y lo isquierda destino, lo que esta a la dereha se copia a la variable nombre.
+nombre = "juan";
+
+//Es para mostrar informacion en una ventana envergente. 
+alert(nombre);
 
